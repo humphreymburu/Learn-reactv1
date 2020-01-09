ea48e2b4f29e9b56f774ccbe15cc69fdd9dbd915
HumphreyMburu <humphrey.asante@gmail.com>
2020-01-09 15:40:12 +0300

    lifting-state
    Lift State

--- src/TemperatureInput.js
+++ src/TemperatureInput.js
@@ -10,15 +10,16 @@ class TemperatureInput extends React.Component {
     super(props);
 
     this.handleChange = this.handleChange.bind(this);
-    this.state = { temperature: "" };
+    //this.state = { temperature: "" };
   }
 
   handleChange(e) {
-    this.setState({ temperature: e.target.value });
+    this.props.onTemperatureChange(e.target.value);
   }
 
   render() {
-    const temperature = this.state.temperature;
+    //const temperature = this.state.temperature;
+    const temperature = this.props.temperature;
     const scale = this.props.scale;
 
     return (
